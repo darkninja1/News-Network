4d6058da42dac2af9d22494f1c6fc84447f0a002
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-12-16 23:45:11 +0000

    ...

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var head1 = ["Games at school"];
-var type = ['pic'];
-var img1 = ['pics/blocked2.png'];
-var head2 = ['Have you Every wondered how games are blocked at school(MCPS)?'];
+var head1 = ["Games at school","Drone Cape Henlopen"];
+var type = ['pic','vid'];
+var img1 = ['pics/blocked2.png','pics/drone1.png'];
+var head2 = ['Have you Every wondered how games are blocked at school(MCPS)?','Sweet view from a $30 drone'];
 var write = ['In MCPS schools there are four different blocking systems in place. The first and most annoying called <i>iboss Cloud</i> this is a blocking system that uses modern AI technolgies to automatically block all sites that have a server intergrated in them, unless whitlisted by tech specialists. The Second most annyoing blocking system is <i>ublock</i> another system put in place by the county. <i>ublock</i> is a small extetion that can be turned off and on depending on the site. Of course you can turn it off but the truth is its consistently running. Now we get to the third gogaurdian, now we all know about gogaurdian. It is a blocking system connected through google that can block and control students computers. GoGaurdian has been implemented for many years now and is probobly the oldest. Fourth and the last is the internet, all schools in MCPS are connected to some form of the countys internet which restricts the sites you can access on thier network. Anyways that about sums up the blocking systems used by MCPS county in school. Thanks! :)'];
-var date1 = ['December 16, 2021'];
+var date1 = ['December 16, 2021','June 23, 2021'];
 var cit1 = ['Quotes from dom'];
 var vids = ['drone1.mp4'];
 var des = ['Novice drone flyer with less than two flights total and a $30 drone :)Cape Henlopen 720px quality...'];
