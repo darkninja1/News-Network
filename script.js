559be8e85c94f42c751267f57a92fa452074a1a0
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-12-16 18:10:57 +0000

    ...

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ var cit1 = ['meh','meg'];
 
 for (var i = 0;i < head1.length;i++) {
   if (type[i] == "vid") {
-    document.getElementById('art123').innerHTML += "<div class='card' onclick='artshow("+head1[i]+")'><h1 class='frontlink'>"+head1[i]+"</h1><div class='container'><img src='"+img1[i]+"' class='mainpic opacityimg'><i class='opacityimg fa fa-play-circle-o centered'></i></img></div>"+head2[i]+"</div>";
+    document.getElementById('art123').innerHTML += "<div class='card' onclick='artshow("+i+")'><h1 class='frontlink'>"+head1[i]+"</h1><div class='container'><img src='"+img1[i]+"' class='mainpic opacityimg'><i class='opacityimg fa fa-play-circle-o centered'></i></img></div>"+head2[i]+"</div>";
   }
   else {
-    document.getElementById('art123').innerHTML += "<div class='card' onclick='artshow("+head1[i]+")'><h1 class='frontlink'>"+head1[i]+"</h1><div class='container'><img src='"+img1[i]+"' class='mainpic opacityimg'></img></div>"+head2[i]+"</div>";
+    document.getElementById('art123').innerHTML += "<div class='card' onclick='artshow("+i+")'><h1 class='frontlink'>"+head1[i]+"</h1><div class='container'><img src='"+img1[i]+"' class='mainpic opacityimg'></img></div>"+head2[i]+"</div>";
   }
 }
 
 function artshow(art) {
-  var theart = head1.indexOf(art);
+  var theart = art;
   document.getElementById('art1').style.display = 'block';
   document.getElementById('head1').innerHTML = head1[theart];
   document.getElementById('head2').innerHTML = head2[theart];
