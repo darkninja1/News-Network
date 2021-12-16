0835f11d19a3d4155f3d842b1123b61f6a7f3b94
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-12-16 23:28:44 +0000

    ...

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var head1 = ["Game's at school"];
+var head1 = ["Games at school"];
 var type = ['pic'];
 var img1 = ['pics/blocked2.png'];
 var head2 = ['Have you Every wondered how games are blocked at school(MCPS)?'];
-var write = ['In MCPS schools there are four different blocking systems in place. The first and most annoying called <i>Iboss Cloud</i> this is a blocking system that uses moder AI technolgies to automatically block all sites that have a server connected, unless whitlisted. The Second most annyoing blocking system is <i>ublock</i> a system put inplace by the county. <i>Ublock</i> is a small extetion that can be turned off and on depending on the site. Of course you can turn it off but the truth is its consistently running. Now we get to the third gogaurdian, now we all know about gogaurdian. It is a blocking system connected through google that can block and control students computers. Fourth and the last is the internet, all schools in MCPS are connected to some form of the schools internet which restricts the sites you can access on thier network. Anyways that about sums up the blocking systems used by MCPS county in school. Thanks! :)'];
+var write = ['In MCPS schools there are four different blocking systems in place. The first and most annoying called <i>iboss Cloud</i> this is a blocking system that uses modern AI technolgies to automatically block all sites that have a server intergrated in them, unless whitlisted by tech specialists. The Second most annyoing blocking system is <i>ublock</i> another system put in place by the county. <i>ublock</i> is a small extetion that can be turned off and on depending on the site. Of course you can turn it off but the truth is its consistently running. Now we get to the third gogaurdian, now we all know about gogaurdian. It is a blocking system connected through google that can block and control students computers. GoGaurdian has been implemented for many years now and is probobly the oldest. Fourth and the last is the internet, all schools in MCPS are connected to some form of the countys internet which restricts the sites you can access on thier network. Anyways that about sums up the blocking systems used by MCPS county in school. Thanks! :)'];
 var date1 = ['December 16, 2021'];
 var cit1 = ['Quotes from dom'];
 var vids = [''];
@@ -14,13 +14,19 @@ var vidsL = vids.length;
 
 
 function artshow(art) {
-  document.getElementById('art1').style.display = 'block';
-  document.getElementById('head1').innerHTML = head1[art];
-  document.getElementById('head2').innerHTML = head2[art];
-  document.getElementById('img1').src = img1[art];
-  document.getElementById('date1').innerHTML = date1[art];
-  document.getElementById('art2').innerHTML = write[art];
-  //document.write(art);
+  if (type[art] == 'vid') {
+    vidsCounter = art;
+    window.location.replace("https://News-Network.dominikhonzak.repl.co/videos.html");
+  }
+  else {
+    document.getElementById('art1').style.display = 'block';
+    document.getElementById('head1').innerHTML = head1[art];
+    document.getElementById('head2').innerHTML = head2[art];
+    document.getElementById('img1').src = img1[art];
+    document.getElementById('date1').innerHTML = date1[art];
+    document.getElementById('art2').innerHTML = write[art];
+    //document.write(art);
+  }
 }
 
 for (var i = 0;i < head1.length;i++) {
